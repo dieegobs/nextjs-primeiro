050e9d7654b85d6dc4736ceede0425471249591a
dieegobs <diegohevant@gmail.com>
2023-02-22 00:05:52 -0300

    Puxando dados jsonplaceholder e listando

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,36 @@
-const Blog = () => {
+import { Post } from '../../types/Post';
+
+
+type Props = {
+    posts: Post[]
+}
+
+
+const Blog = ( {posts}: Props) => {
     return (
         <div>
             <h1>Blog</h1>
+
+            <ul>
+                {posts.map((post, index) => (
+                    <li key={index}> {post.title} </li>
+                ))}
+            </ul>
         </div>
     );
 }
+
+export const getStaticProps = async() => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts: Post[] = await res.json();
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
+
 
 export default Blog;
