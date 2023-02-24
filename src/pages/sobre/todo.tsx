21e8235df07d3e5198480b4f1696ed5b0a3eb1ca
dieegobs <diegohevant@gmail.com>
2023-02-23 22:43:10 -0300

    Criando o types Todo

--- src/pages/sobre/todo.tsx
+++ src/pages/sobre/todo.tsx
@@ -1,1 +1,36 @@
 import { useEffect, useState } from "react";
+import { Todo } from "@/types/Todo";
+
+const Todo = () => {
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=> {
+        loadTodos();
+    }, []);
+
+    const loadTodos = async () => {
+        setLoading(true);
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const list: Todo[] = await res.json();
+        setTodoList(list);
+        setLoading(false);
+    }
+
+    return ( 
+        <div>
+            <h1>Lista de Tarefas</h1>
+
+            {loading && <div> Carregando... </div>}
+
+            <ul>
+                {todoList.map((todoItem, index) => (
+                    <li key={index}>{todoItem.title} - {todoItem.completed.toString()}</li>
+                ))}
+            </ul>
+
+        </div>
+    );
+}
+
+export default Todo;
