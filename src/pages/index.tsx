51c372426cb8b9257dbc23d7475d88fbd8aae0ef
dieegobs <diegohevant@gmail.com>
2023-02-26 20:04:40 -0300

    Usando o link do next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Logo from '../pages/assets/ebaylogo.png'
-
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,6 +10,17 @@ export default function Home() {
         alt='Logo Ebay'
         quality={100}
       />
+
+      <ul>
+        <li> 
+          <Link href="/about">About</Link>
+        </li>
+        <li> 
+          <Link href="/contact">Contact</Link>
+          
+        </li>
+
+      </ul>
     </div>
   )
 }
